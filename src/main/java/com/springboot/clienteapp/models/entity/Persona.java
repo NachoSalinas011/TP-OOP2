738418a7d1f06aec25470bcbedf1c16fc423de85
FedeFrumboli <federico.n.frumboli@gmail.com>
2022-05-16 01:16:40 -0300

    Creacion entity Persona (conexion con tabla bd persona)

---
+++ src/main/java/com/springboot/clienteapp/models/entity/Persona.java
@@ -0,0 +1,62 @@
+package com.springboot.clienteapp.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="persona")
+public class Persona implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idPersona;
+	private String nombre;
+	private String apellido;
+	private String tipoDoc;
+	private int nroDoc;
+	public int getIdPersona() {
+		return idPersona;
+	}
+	protected void setIdPersona(int idPersona) {
+		this.idPersona = idPersona;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getTipoDoc() {
+		return tipoDoc;
+	}
+	public void setTipoDoc(String tipoDoc) {
+		this.tipoDoc = tipoDoc;
+	}
+	public int getNroDoc() {
+		return nroDoc;
+	}
+	public void setNroDoc(int nroDoc) {
+		this.nroDoc = nroDoc;
+	}
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", tipoDoc=" + tipoDoc + ", nroDoc=" + nroDoc
+				+ "]";
+	}
+	
+//	
+	
+	
+}
