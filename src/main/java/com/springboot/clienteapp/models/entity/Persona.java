ab070c36b6100f0e6a36220a7d0519a3c6f53209
FedeFrumboli <federico.n.frumboli@gmail.com>
2022-05-16 16:58:22 -0300

    modificacion entidad persona, creacion entidad usuario)

--- src/main/java/com/springboot/clienteapp/models/entity/Persona.java
+++ src/main/java/com/springboot/clienteapp/models/entity/Persona.java
@@ -9,54 +9,73 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="persona")
+@Table(name = "persona")
 public class Persona implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idPersona;
 	private String nombre;
 	private String apellido;
 	private String tipoDoc;
 	private int nroDoc;
+	private boolean baja;
+
 	public int getIdPersona() {
 		return idPersona;
 	}
+
 	protected void setIdPersona(int idPersona) {
 		this.idPersona = idPersona;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
 	public String getTipoDoc() {
 		return tipoDoc;
 	}
+
 	public void setTipoDoc(String tipoDoc) {
 		this.tipoDoc = tipoDoc;
 	}
+
 	public int getNroDoc() {
 		return nroDoc;
 	}
+
 	public void setNroDoc(int nroDoc) {
 		this.nroDoc = nroDoc;
 	}
+
+	public boolean isBaja() {
+		return baja;
+	}
+
+	public void setBaja(boolean baja) {
+		this.baja = baja;
+	}
+
 	@Override
 	public String toString() {
-		return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", tipoDoc=" + tipoDoc + ", nroDoc=" + nroDoc
-				+ "]";
+		return "Persona [idPersona=" + idPersona + ", nombre=" + nombre + ", apellido=" + apellido + ", tipoDoc="
+				+ tipoDoc + ", nroDoc=" + nroDoc + ", baja=" + baja + "]";
 	}
-	
+
 //	
-	
-	
+
 }
---
+++ src/main/java/com/springboot/clienteapp/models/entity/Usuario.java
@@ -0,0 +1,86 @@
+package com.springboot.clienteapp.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idUsuario;
+
+	@OneToOne
+	@JoinColumn(name = "idPersona")
+
+	private Persona persona;
+	private String correo;
+	private String nombreUsuario;
+	private String password;
+	private boolean baja;
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	protected void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public Persona getPersona() {
+		return persona;
+	}
+
+	public void setPersona(Persona persona) {
+		this.persona = persona;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isBaja() {
+		return baja;
+	}
+
+	public void setBaja(boolean baja) {
+		this.baja = baja;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [idUsuario=" + idUsuario + ", persona=" + persona + ", correo=" + correo + ", nombreUsuario="
+				+ nombreUsuario + ", password=" + password + ", baja=" + baja + "]";
+	}
+
+}
